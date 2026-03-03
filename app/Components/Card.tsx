5a051d1e5411f59535ead915d47f3bbd8480227b
UP240104 <up240104@alumnos.upa.edu.mx>
2026-03-02 18:11:15 -0600

    add getPokemons

---
+++ app/Components/Card.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Card({ name }: { name: string }) {
+  return (
+    <View className="bg-white border border-gray-200 rounded-lg shadow-md p-4 m-2">
+      <Text className="text-lg font-bold">{name}</Text>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import Card from "./Components/Card";
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+      const res = await fetch(URL, {
+        method: "GET",
+      });
+      const data = await res.json();
+      setPokemons(data.results);
+    };
+    getPokemons();
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white" grid-cols-1 gap-4 p-4>
+      {pokemons.map((pokemon) => (
+        <Card key={pokemon.name} name={pokemon.name} />
+      ))}
+    </ScrollView>
   );
 }
